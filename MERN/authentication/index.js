53c4a6754afeebc8980aeebe2e0288deaa99be41
LOVISH066 <lovishgoyal066@gmail.com>
2025-02-19 22:51:22 +0530

    update

---
+++ MERN/authentication/index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+const users = [];
+
+function generateToken() {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let token = "";
+  for (let i = 0; i < 16; i++) {
+    token += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return token;
+}
+
+app.post("/signup", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  users.push({
+    username: username,
+    password: password,
+  });
+
+  res.json({
+    message: "Successfully Signed Up",
+  });
+});
+
+app.post("/signin", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  let foundUser = null;
+
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].username == username && users[i].password == password) {
+      foundUser = users[i];
+    }
+  }
+
+  if (foundUser) {
+    const token = generateToken();
+    foundUser.token = token;
+    res.json({
+      token: token,
+    });
+  } else {
+    res.status(403).send({
+      message: "Invalid Username or Password",
+    });
+  }
+});
+
+app.listen(3000);
